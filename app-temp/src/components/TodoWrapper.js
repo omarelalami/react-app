eae08bafcc253cde48cb29de99086493f1697c77
omarelalami <omaralami230@gmail.com>
2024-02-23 01:28:03 +0100

    create comp and view for comp

--- app-temp/src/components/TodoWrapper.js
+++ app-temp/src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 
 export const TodoWrapper = () => {
+    const []=''
   return (
     <div className='TodoWrapper'>
         <TodoForm></TodoForm>
